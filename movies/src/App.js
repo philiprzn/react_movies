a4d26a6b9dff1504884fa094dc8996ff3497c004
Philip <philip_bmx11@yahoo.com>
2020-12-03 21:13:34 +0300

    hw1 all components created;

--- movies/src/App.js
+++ movies/src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import Pure from "./HW1/PureComponent";
 import ClassComponent from "./HW1/ClassComponent";
 import FunctionalComponent from "./HW1/FunctionalComponent";
+import CreateElement from "./HW1/CreateElement";
 
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello React</p>
-      <FunctionalComponent/>
-      <ClassComponent/>
-      <Pure/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>Hello React</p>
+            {CreateElement}
+            <FunctionalComponent/>
+            <ClassComponent/>
+            <Pure/>
+        </div>
+    );
 }
 
 export default App;
---
+++ movies/src/HW1/CreateElement.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CreateElement = React.createElement('p', null, 'Hello from createElement');
+
+export default CreateElement;
