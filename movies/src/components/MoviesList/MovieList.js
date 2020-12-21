cca7090a7ea1fb7684b39dc046b558340fbaab49
Philip <philip_bmx11@yahoo.com>
2020-12-21 22:16:54 +0300

    HW3 pass key prop;

--- movies/src/components/MoviesList/MovieList.js
+++ movies/src/components/MoviesList/MovieList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './movieList.css';
+import './movieList.css';
 import MovieCard from "../MovieCard/MovieCard";
 import movies from './../../../movies';
 
 const MovieList = () => {
     const Movies = movies.map(({ title, description, id }) =>
-        <MovieCard title={title} description={description} id={id}/>
+        <MovieCard title={title} description={description} key={id}/>
     );
 
     return (
